55f54df6aaafae6ddbc57bbe61b06e492f298ec7
Hannah TM <hannahtm1@hotmail.com>
2024-05-13 14:28:38 +0100

    initial set up and first api call

---
+++ db_utils.py
@@ -0,0 +1,16 @@
+import mysql.connector
+from config import (user, password, host)
+
+class DbConnectionError(Exception):
+    pass
+
+def _connect_to_db(db_name):
+    connection = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        auth_plugin='mysql_native_password',
+        database=db_name
+    )
+    return connection
+
