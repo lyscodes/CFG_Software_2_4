e84354dd20d3bcf434bf3f657c20ca496bdffe94
Fabiola Palmas <fabiola.palmas@gmail.com>
2024-05-13 16:48:07 +0100

    Created templates for mood and quote html pages, connected routes to the pages.

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from main import get_quote_otd
 # from db_utils import 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/home')
+@app.route('/mood')
+def mood_checkin():
+    my_mood = 'happy'
+    return render_template("mood.html", mood=my_mood)
+
+
+@app.route('/quote')
 def quote_of_the_day():
     result = get_quote_otd()
     quote = result[0]
     author = result[1]
-    return f'Quote of the day is: {quote} By {author}'
+    return render_template("quote.html", quote=f'Quote of the day is: {quote} By {author}')
+
+
 
 
 
