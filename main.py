5a2ba3856114ba0ca2d0f85e8c08fec2368fd57f
Rachel Tookey <rachel.tookey@gmail.com>
2024-05-14 20:45:56 +0100

    First attempt at a get_moods endpoint, which takes a specified mood as a string as an argument

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import requests
+from config import key
 import json
-
+from urllib import parse, request
+from random import randint
 
 endpoint1 = 'https://zenquotes.io/api/today'
+
+
+def get_moods(mood):
+    url = "http://api.giphy.com/v1/gifs/search"
+    offset = randint(0, 4999) # this means a new result will show up each time the call is made
+    params = parse.urlencode({
+        "q": mood, # set up to take input of call for mood
+        "api_key": key, # key in config file
+        "limit": "1", # returns on one result
+        "offset": offset
+    })
+    try:
+        with request.urlopen("".join((url, "?", params))) as response:
+            data = json.loads(response.read())
+            list = data['data']
+            item = list[0]
+            gif_url = item['url']
+            print(gif_url)
+            return gif_url
+    except Exception:
+        print("Opps: error")
+
+
 
 def get_quote_otd():
     result = requests.get(endpoint1).json()
@@ -29,5 +54,9 @@ def get_quotes_by_keyword():
 get_quotes_by_keyword()
 """
 
-# if __name__ == '__main__':
-#     run()
+
+
+
+if __name__ == '__main__':
+     mood = "sad"
+     get_moods(mood)
