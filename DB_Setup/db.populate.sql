6f97e447e3608df4329db4ae4e5be8dc9b8b014d
Hannah TM <hannahtm1@hotmail.com>
2024-06-02 18:35:40 +0100

    dummy data and database fixes

--- DB_Setup/db.populate.sql
+++ DB_Setup/db.populate.sql
@@ -1,6 +1,47 @@
 USE Mood_Tracker;
 
+USE Mood_Tracker;
+
 INSERT INTO Users
-(First_Name, Family_Name, User_Name, Password, Profile_Pic)
+(First_Name, Family_Name, User_Name, Email, Password)
 VALUES
-('John', 'Doe', 'JoDoe', 'password123', 'my_pic');
+('John', 'Doe', 'JoDoe', 'johndoes@example.com', 'password123'),
+('Lucy', 'Smith', 'LSmith', 'lsmith@example.com', 'hello123');
+
+
+INSERT INTO Entries
+(User_ID, Entry_Date, Emotion, Giph_URL, Choice_J_or_Q, Response_J_or_Q, Diary_Entry)
+VALUES
+(1, 20240601, 'happy', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel happy!'),
+(2, 20240601, 'worried', 'Giph_URL', 'Test choice', 'Test response', 'I am worried!'),
+(1, 20240531, 'frustrated', 'Giph_URL', 'Test choice', 'Test response', 'Super frustrated!'),
+(1, 20240530, 'sad', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel sad!'),
+(1, 20240529, 'worried', 'Giph_URL', 'Test choice', 'Test response', 'I am worried!'),
+(1, 20240528, 'worried', 'Giph_URL', 'Test choice', 'Test response', 'I am worried!'),
+(1, 20240527, 'happy', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel happy!'),
+(1, 20240526, 'frustrated', 'Giph_URL', 'Test choice', 'Test response', 'Super frustrated!'),
+(1, 20240525, 'happy', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel happy!'),
+(1, 20240524, 'happy', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel happy!'),
+(1, 20240523, 'calm', 'Giph_URL', 'Test choice', 'Test response', 'Zen!'),
+(1, 20240522, 'frustrated', 'Giph_URL', 'Test choice', 'Test response', 'Super frustrated!'),
+(1, 20240521, 'worried', 'Giph_URL', 'Test choice', 'Test response', 'I am worried!'),
+(1, 20240520, 'angry', 'Giph_URL', 'Test choice', 'Test response', 'GRRRR!'),
+(1, 20240519, 'sad', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel sad!'),
+(1, 20240518, 'happy', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel happy!'),
+(1, 20240517, 'calm', 'Giph_URL', 'Test choice', 'Test response', 'Zen!'),
+(1, 20240516, 'happy', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel happy!'),
+(1, 20240515, 'calm', 'Giph_URL', 'Test choice', 'Test response', 'Zen!'),
+(1, 20240514, 'angry', 'Giph_URL', 'Test choice', 'Test response', 'GRRRR!'),
+(1, 20240513, 'angry', 'Giph_URL', 'Test choice', 'Test response', 'GRRRR!'),
+(1, 20240512, 'worried', 'Giph_URL', 'Test choice', 'Test response', 'I am worried!'),
+(1, 20240511, 'worried', 'Giph_URL', 'Test choice', 'Test response', 'I am worried!'),
+(1, 20240510, 'happy', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel happy!'),
+(1, 20240509, 'calm', 'Giph_URL', 'Test choice', 'Test response', 'Zen!'),
+(1, 20240508, 'calm', 'Giph_URL', 'Test choice', 'Test response', 'Zen!'),
+(1, 20240507, 'happy', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel happy!'),
+(1, 20240506, 'calm', 'Giph_URL', 'Test choice', 'Test response', 'Zen!'),
+(1, 20240505, 'calm', 'Giph_URL', 'Test choice', 'Test response', 'Zen!'),
+(1, 20240504, 'sad', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel sad!'),
+(1, 20240503, 'frustrated', 'Giph_URL', 'Test choice', 'Test response', 'Super frustrated!'),
+(1, 20240502, 'frustrated', 'Giph_URL', 'Test choice', 'Test response', 'Super frustrated!'),
+(1, 20240501, 'happy', 'Giph_URL', 'Test choice', 'Test response', 'Today I feel happy!');
--- DB_Setup/db_create.sql
+++ DB_Setup/db_create.sql
@@ -9,16 +9,19 @@ CREATE TABLE Users (
     First_Name VARCHAR(50) NOT NULL,
 	Family_Name VARCHAR(50) NOT NULL,
 	User_Name VARCHAR(50) NOT NULL UNIQUE,
-	Password VARCHAR(50) NOT NULL,
-	Profile_Pic VARCHAR(50) NOT NULL
+    Email VARCHAR(50) NOT NULL UNIQUE,
+	Password VARCHAR(50) NOT NULL
 );
 
 -- create table that stores entries in mood tracker app
 CREATE TABLE Entries (
 	ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    User_Name VARCHAR(50) NOT NULL UNIQUE,
+    User_ID INT NOT NULL,
     Entry_Date DATE NOT NULL,
     Emotion VARCHAR(50),
-    Choice VARCHAR(50),
-    Response VARCHAR(500),
-    Diary_Entry VARCHAR(500));
+    Giph_URL VARCHAR(250),
+    Choice_J_or_Q VARCHAR(50),
+    Response_J_or_Q VARCHAR(500),
+    Diary_Entry VARCHAR(500),
+    CONSTRAINT FK_Users FOREIGN KEY (User_ID) REFERENCES Users(ID)
+);
