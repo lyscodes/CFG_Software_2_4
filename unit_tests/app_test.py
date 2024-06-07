37370e6985914134fea0524c606f32ac8cc3e607
Tookey <rachel.tookey@gmail.com>
2024-06-07 22:04:23 +0100

    Added in some final testing.

--- unit_tests/app_test.py
+++ unit_tests/app_test.py
@@ -12,26 +12,26 @@ class MyTest(TestCase):
         return app
 
     def test_template_rendered(self):
-        response = self.client.get('/')
+        self.client.get('/')
         self.assert_template_used('mood.html')
 
     def test_template_rendered_quote(self):
-        response = self.client.get('/quote')
+        self.client.get('/quote')
         self.assert_template_used('quote.html')
 
     def test_template_rendered_choice(self):
         with self.client.session_transaction() as test_sess:
             test_sess['mood_dict'] = {'sad': ''}
 
-        response = self.client.get('/choice/sad')
+        self.client.get('/choice/sad')
         self.assert_template_used('choice.html')
 
     def test_template_rendered_joke(self):
-        response = self.client.get('/joke')
+        self.client.get('/joke')
         self.assert_template_used('joke.html')
 
     def test_template_rendered_journal(self):
-        response = self.client.get('/journal')
+        self.client.get('/journal')
         self.assert_template_used('journal.html')
 
     def test_overview_redirect(self): # check it redirects if not logged in
@@ -39,11 +39,11 @@ class MyTest(TestCase):
         self.assert_status(response, 302)
 
     def test_form_submission_wrongpw(self): # check password validaiton and flash messages
-        response = self.client.post('/register', data={'FirstName': 'Rachel', 'LastName': 'Tookey', "Username": "Rachel1993", "email": "rachel@tookey.com", "password":"snow", "confirm":"rain", "accept_tos":True})
+        self.client.post('/register', data={'FirstName': 'Rachel', 'LastName': 'Tookey', "Username": "Rachel1993", "email": "rachel@tookey.com", "password":"snow", "confirm":"rain", "accept_tos":True})
         self.assert_message_flashed('Password and Password Confirmation do not match', "error")
 
     def test_username(self): # check password validaiton and flash messages
-        response = self.client.post('/register', data={'FirstName': 'Rachel', 'LastName': 'Tookey', "Username": "JoDoe", "email": "r@tookey.com", "password":"snow", "confirm":"snow", "accept_tos":True})
+        self.client.post('/register', data={'FirstName': 'Rachel', 'LastName': 'Tookey', "Username": "JoDoe", "email": "r@tookey.com", "password":"snow", "confirm":"snow", "accept_tos":True})
         self.assert_message_flashed("Username already in use", "error")
 
 
@@ -56,31 +56,37 @@ class MyTest(TestCase):
         response = self.client.get('/hello')
         self.assert404(response)
 
-    def test_login_logic(self):
-        response = self.client.get('/overview')
-        self.assert_status(response, 302)
-
+    def test_login_200(self):
         response = self.client.get('/login')
-        self.assert_status(response, 200)
+        self.assert_200(response, 200)
 
+    def test_session(self):
         with self.client.session_transaction() as test_sess:
             test_sess['user'] = 'testuser'
+        self.assertIn('user', test_sess)
 
-        response = self.client.get('/overview')
-        self.assert200(response)
+    def test_login_overview(self):
+        with self.client:
+            with self.client.session_transaction() as session:
+                session['user'] = 'testuser'
+            response = self.client.get('/overview')
+            self.assert200(response)
 
-    def test_logout_logic(self):
-        with self.client.session_transaction() as test_sess:
-            test_sess['user'] = 'testuser'
-
+    def test_logout(self):
         response = self.client.get('/logout')
         self.assert_status(response, 302)
 
-        response = self.client.get('/overview')
-        self.assert_status(response, 302)
+    def test_logout_flash(self):
+        self.client.get('/logout')
+        self.assert_message_flashed("You have been logged out. See you soon!", "notification")
 
-        with self.client.session_transaction() as test_sess:
-           self.assertNotIn('user', test_sess)
+    def test_logout_session(self):
+        with self.client:
+            with self.client.session_transaction() as test_sess:
+                test_sess['user'] = 'testuser'
+            self.client.get('/logout')
+            with self.client.session_transaction() as test_sess:
+                self.assertNotIn('user', test_sess)
 
 if __name__ == "__main__":
     unittest.main()
