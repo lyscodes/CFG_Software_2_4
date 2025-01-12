1d61f5ec0d81a17cb553b10a5f4f0a07b85a6406
Tookey <rachel.tookey@gmail.com>
2025-01-12 15:31:22 +0000

    Feat: created flask migrate file including dummy data

--- data/user_data.py
+++
@@ -1,6 +0,0 @@
-
-users_dd = [('JoDoe',  "johndoes@example.com"),
-            ('LSmith', "lsmith@example.com")]
-
-localusers_dd = [(1, 'John', 'Doe',  "$2b$12$.VxfEmaBgIMwLNuhfZxmVOXjfo2Xm8K5i1bADqYJmCSu/aTst6IAW", True),
-                 (2, 'Lucy', 'Smith',"$2b$12$6Y6aUmThOUKmf0DHxEv26OiL/5YfAPMzCLzjKoZ6lHhBW5u3hn0ym", True)]
--- run.py
+++ run.py
@@ -1,18 +1,11 @@
-from app import create_app, db
-from dotenv import load_dotenv
-from sqlalchemy_utils import database_exists, create_database
+from app import create_app
 import os
-from sqlalchemy import create_engine
-from app.db_init import initialize_dummy_data
 from socket import gethostname
 
 app = create_app()
 
 if __name__ == "__main__":
     os.environ['FLASK_ENV'] = 'development'
-    #
-    # with app.app_context():
-    #     initialize_dummy_data()
 
     if 'liveconsole' not in gethostname():
         app.run(debug=True, ssl_context=('certs/certificate.pem', 'certs/private.pem'), host='0.0.0.0', port=443)
